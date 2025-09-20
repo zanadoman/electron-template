393c01c593fea9dd2589bc9d59a9bea744a18672
zanadoman <domanseven@gmail.com>
2025-09-20 10:42:35 +0200

    angular setup

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -20,6 +20,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, electron-template');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Electron');
   });
 });
--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('electron-template');
+  protected readonly title = signal('Electron');
 }
